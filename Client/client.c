9e602a5586485b98b2a5e78c424dbfa1b5df245a
Fabrice DEDO <dsamfabrice5@gmail.com>
2022-06-07 15:43:46 +0200

    WIP

---
+++ Client/client.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
+#include <poll.h>
+
+#include "client.h"
+#include <libsmtp.h>
+#include <libcom.h>
+#include <resolv.h>
+#include <arpa/inet.h>
+
+
+int main(int argc,char *argv[])
+{
+	int s;
+
+	/* Lecture des arguments de la commande */
+	//analyseArguments(argc,argv);
+	char *port = "4000";
+	char *machine = "127.0.0.1";
+	/* Connection au serveur */
+	s=connexionServeur(machine,port);
+	if(s<0){ fprintf(stderr,"Erreur de connexion au serveur\n"); exit(EXIT_FAILURE); }
+
+	/* Boucle de communication avec le serveur */
+	struct pollfd descripteurs[2];
+	descripteurs[0].fd=s;
+	descripteurs[0].events=POLLIN;
+	descripteurs[1].fd=0;
+	descripteurs[1].events=POLLIN;
+	while(1){
+  		char tampon[MAX_TAMPON];
+  		int nb=poll(descripteurs,2,-1);
+  		if(nb<0){ perror("main.poll"); exit(EXIT_FAILURE); }
+  		if((descripteurs[0].revents&POLLIN)!=0){
+    		int taille=read(s,tampon,MAX_TAMPON);
+    		if(taille<=0) break;
+    		write(1,tampon,taille);
+    		}
+  		if((descripteurs[1].revents&POLLIN)!=0){
+    		int taille=read(0,tampon,MAX_TAMPON);
+    		if(taille<=0) break;
+    		write(s,tampon,taille);
+    		}
+  	}
+
+	//resolution_DNS(machine);
+	/* On termine la connexion */
+	shutdown(s,SHUT_RDWR);
+
+	return 0;
+}
---
+++ Client/client.h
@@ -0,0 +1,16 @@
+#ifndef __MTAINT_H__
+#define __MTAINT_H__
+
+/******************************************************************/
+/** Ce fichier decrit les structures et les constantes utilisees **/
+/** par les fonctions de gestion des sockets                     **/
+/******************************************************************/
+
+/**** Constantes ****/
+
+#define MAX_TAMPON 12000
+
+/**** Fonctions ****/
+
+
+#endif // !__MTAINT_H__
